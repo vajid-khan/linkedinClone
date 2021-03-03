49c90d2732edef22d8c09813f3031b91e2f6a74d
vajid khan <khanvajid04@gmail.com>
2021-03-03 14:02:20 +0400

    network scren

--- src/screens/networks/networks.tsx
+++ src/screens/networks/networks.tsx
@@ -1,13 +1,65 @@
 import React from 'react';
-import {View, Text} from 'react-native';
+import {ScrollView} from 'react-native';
+import {Box, Text, Theme} from '../../theme';
+import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
+import {useTheme} from '@shopify/restyle';
+import User from './userCard';
 
 interface Props {}
 
 const Networks: React.FC<Props> = () => {
+  const theme = useTheme<Theme>();
+
   return (
-    <View>
-      <Text>Networks</Text>
-    </View>
+    <ScrollView
+      contentContainerStyle={{
+        backgroundColor: theme.colors.background,
+      }}>
+      <Box
+        padding={'sm'}
+        flexDirection={'row'}
+        backgroundColor={'light'}
+        justifyContent={'space-between'}
+        alignItems={'center'}>
+        <Text color={'primary'} fontSize={20}>
+          Manage my network
+        </Text>
+        <Icon name={'chevron-right'} size={30} />
+      </Box>
+      <Box
+        marginTop={'s'}
+        padding={'sm'}
+        flexDirection={'row'}
+        backgroundColor={'light'}
+        justifyContent={'space-between'}
+        alignItems={'center'}>
+        <Text color={'primary'} fontSize={20}>
+          Invitations
+        </Text>
+        <Icon name={'chevron-right'} size={30} />
+      </Box>
+
+      <Box marginTop={'s'} backgroundColor={'light'}>
+        <Text padding={'s'} fontSize={18}>
+          People you may know from your university
+        </Text>
+        <Box flexWrap={'wrap'} flexDirection={'row'}>
+          {new Array(6).fill(0).map((_, index) => (
+            <User key={index} />
+          ))}
+        </Box>
+      </Box>
+      <Box marginTop={'s'} backgroundColor={'light'}>
+        <Text padding={'s'} fontSize={18}>
+          Software Engineer you may know
+        </Text>
+        <Box flexWrap={'wrap'} flexDirection={'row'}>
+          {new Array(9).fill(0).map((_, index) => (
+            <User key={index} />
+          ))}
+        </Box>
+      </Box>
+    </ScrollView>
   );
 };
 export default Networks;
---
+++ src/screens/networks/userCard.tsx
@@ -0,0 +1,48 @@
+import React from 'react';
+import {Dimensions} from 'react-native';
+import Avatar from '../../components/avatar';
+import {Box, Text} from '../../theme';
+import Icon from 'react-native-vector-icons/Feather';
+
+const {width} = Dimensions.get('screen');
+
+interface Props {}
+
+const User: React.FC<Props> = () => {
+  return (
+    <Box style={{width: width / 2}}>
+      <Box margin={'s'}>
+        <Box
+          height={100}
+          backgroundColor={'primary'}
+          borderTopLeftRadius={15}
+          borderTopRightRadius={15}
+        />
+        <Box
+          alignItems={'center'}
+          style={{
+            top: -50,
+          }}>
+          <Avatar size={100} />
+          <Text>Jhon Doe</Text>
+          <Text>Full Stack Developer</Text>
+          <Box marginVertical={'s'} flexDirection={'row'} alignItems={'center'}>
+            <Icon name={'user-plus'} size={20} />
+            <Text>10 mutual connections</Text>
+          </Box>
+          <Box
+            paddingVertical={'s'}
+            paddingHorizontal={'l'}
+            borderRadius={50}
+            marginRight={'s'}
+            marginBottom={'s'}
+            borderColor={'primary'}
+            borderWidth={2}>
+            <Text color={'primary'}>Connect</Text>
+          </Box>
+        </Box>
+      </Box>
+    </Box>
+  );
+};
+export default User;
