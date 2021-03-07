822da6e5f23e600f2f65defa4f8459b82de68608
vajid khan <khanvajid04@gmail.com>
2021-03-07 19:44:00 +0400

    network card  ui fix

--- src/screens/networks/userCard.tsx
+++ src/screens/networks/userCard.tsx
@@ -1,50 +1,50 @@
 import React from 'react';
 import {Dimensions} from 'react-native';
 import Avatar from '../../components/avatar';
-import {Box, Text} from '../../theme';
+import {Box, Text, Theme} from '../../theme';
 import Icon from 'react-native-vector-icons/Feather';
+import {useTheme} from '@shopify/restyle';
 
 const {width} = Dimensions.get('screen');
 
 interface Props {}
 
 const User: React.FC<Props> = () => {
+  const theme = useTheme<Theme>();
+
   return (
-    <Box style={{width: width / 2}}>
-      <Box margin={'s'} borderColor={'background'}>
-        <Box
-          height={100}
-          backgroundColor={'background'}
-          borderTopLeftRadius={15}
-          borderTopRightRadius={15}
-        />
-        <Box
-          alignItems={'center'}
-          borderRightWidth={1}
-          borderLeftWidth={1}
-          borderBottomWidth={1}
-          borderColor={'background'}
-          style={{
-            top: -50,
-          }}>
-          <Avatar size={100} />
-          <Text>Jhon Doe</Text>
-          <Text>Full Stack Developer</Text>
-          <Box marginVertical={'s'} flexDirection={'row'} alignItems={'center'}>
-            <Icon name={'user-plus'} size={20} />
-            <Text>10 mutual connections</Text>
-          </Box>
-          <Box
-            paddingVertical={'s'}
-            paddingHorizontal={'l'}
-            borderRadius={50}
-            marginRight={'s'}
-            marginBottom={'l'}
-            borderColor={'primary'}
-            borderWidth={2}>
-            <Text color={'primary'}>Connect</Text>
-          </Box>
+    <Box
+      style={{width: width / 2 - theme.spacing.s * 2}}
+      borderRadius={15}
+      borderWidth={1}
+      margin={'s'}
+      overflow={'hidden'}
+      borderColor={'background'}>
+      <Box height={100} backgroundColor={'background'} />
+      <Box
+        alignItems={'center'}
+        style={{
+          top: -50,
+        }}>
+        <Avatar size={100} />
+        <Text>Jhon Doe</Text>
+        <Text>Full Stack Developer</Text>
+        <Box marginVertical={'s'} flexDirection={'row'} alignItems={'center'}>
+          <Icon name={'user-plus'} size={20} />
+          <Text>10 mutual connections</Text>
         </Box>
+      </Box>
+      <Box
+        borderRadius={50}
+        marginBottom={'l'}
+        paddingVertical={'s'}
+        paddingHorizontal={'l'}
+        marginHorizontal={'m'}
+        borderColor={'primary'}
+        alignItems={'center'}
+        borderWidth={2}
+        style={{marginTop: -25}}>
+        <Text color={'primary'}>Connect</Text>
       </Box>
     </Box>
   );
