897d34d45c742297fba192452964e236c6dc58a0
vajid khan <khanvajid04@gmail.com>
2021-03-12 14:39:02 +0400

    added reanimated 2

--- src/routes/tabs/bottomtabs.tsx
+++ src/routes/tabs/bottomtabs.tsx
@@ -1,9 +1,15 @@
+/* eslint-disable react-native/no-inline-styles */
 import React from 'react';
 import {Box, Text, Theme} from '../../theme';
 import {useTheme} from '@shopify/restyle';
-import {TouchableOpacity} from 'react-native';
+import {Dimensions, TouchableOpacity} from 'react-native';
 import Icon from 'react-native-vector-icons/Feather';
 import {BottomTabBarProps} from '@react-navigation/bottom-tabs';
+import Animated, {
+  useAnimatedStyle,
+  useSharedValue,
+  withTiming,
+} from 'react-native-reanimated';
 
 const TabsIcon = {
   Home: 'home',
@@ -13,77 +19,94 @@ const TabsIcon = {
   Jobs: 'shopping-bag',
 };
 
-const BottomTabs: React.FC<BottomTabBarProps> = ({
-  state,
-  descriptors,
-  navigation,
-}) => {
+const SCEEN_WIDTH = Dimensions.get('screen').width;
+
+const BottomTabs: React.FC<BottomTabBarProps> = (props) => {
   const theme = useTheme<Theme>();
+  const {state, descriptors, navigation} = props;
+  const width = SCEEN_WIDTH / state.routes.length;
+  const topBorderTranslateY = useSharedValue(0);
+  const animatedStyle = useAnimatedStyle(() => {
+    return {
+      transform: [{translateX: topBorderTranslateY.value}],
+    };
+  });
+
   return (
-    <Box flexDirection={'row'}>
-      {state.routes.map((route, index) => {
-        const {options} = descriptors[route.key];
-        const label =
-          options.tabBarLabel !== undefined
-            ? options.tabBarLabel
-            : options.title !== undefined
-            ? options.title
-            : route.name;
+    <>
+      <Animated.View
+        style={[
+          {
+            backgroundColor: theme.colors.primary,
+            width,
+            height: 2,
+          },
+          animatedStyle,
+        ]}
+      />
 
-        const isFocused = state.index === index;
+      <Box flexDirection={'row'}>
+        {state.routes.map((route, index) => {
+          const {options} = descriptors[route.key];
+          const label =
+            options.tabBarLabel !== undefined
+              ? options.tabBarLabel
+              : options.title !== undefined
+              ? options.title
+              : route.name;
 
-        const onPress = () => {
-          const event = navigation.emit({
-            type: 'tabPress',
-            target: route.key,
-            canPreventDefault: true,
-          });
+          const isFocused = state.index === index;
 
-          if (!isFocused && !event.defaultPrevented) {
-            navigation.navigate(route.name);
-          }
-        };
+          const onPress = () => {
+            topBorderTranslateY.value = withTiming(width * index);
+            const event = navigation.emit({
+              type: 'tabPress',
+              target: route.key,
+              canPreventDefault: true,
+            });
 
-        const onLongPress = () => {
-          navigation.emit({
-            type: 'tabLongPress',
-            target: route.key,
-          });
-        };
+            if (!isFocused && !event.defaultPrevented) {
+              navigation.navigate(route.name);
+            }
+          };
 
-        return (
-          <Box
-            flex={1}
-            key={index}
-            paddingVertical={'s'}
-            alignItems={'center'}
-            justifyContent={'center'}
-            borderTopWidth={isFocused ? 2 : 0}
-            style={{
-              borderTopColor: '#000',
-            }}>
-            <TouchableOpacity
-              accessibilityRole="button"
-              accessibilityLabel={options.tabBarAccessibilityLabel}
-              testID={options.tabBarTestID}
-              onPress={onPress}
-              onLongPress={onLongPress}
-              style={{alignItems: 'center'}}>
-              <Icon
-                name={TabsIcon[route.name]}
-                size={20}
-                color={
-                  isFocused ? theme.colors.primary : theme.colors.background
-                }
-              />
-              <Text numberOfLines={1} fontSize={12}>
-                {label}
-              </Text>
-            </TouchableOpacity>
-          </Box>
-        );
-      })}
-    </Box>
+          const onLongPress = () => {
+            navigation.emit({
+              type: 'tabLongPress',
+              target: route.key,
+            });
+          };
+
+          return (
+            <Box
+              flex={1}
+              key={index}
+              paddingVertical={'s'}
+              alignItems={'center'}
+              justifyContent={'center'}>
+              <TouchableOpacity
+                accessibilityRole="button"
+                accessibilityLabel={options.tabBarAccessibilityLabel}
+                testID={options.tabBarTestID}
+                onPress={onPress}
+                onLongPress={onLongPress}
+                style={{alignItems: 'center'}}>
+                <Icon
+                  name={TabsIcon[route.name]}
+                  size={20}
+                  color={
+                    isFocused ? theme.colors.primary : theme.colors.background
+                  }
+                />
+                <Text numberOfLines={1} fontSize={12}>
+                  {label}
+                </Text>
+              </TouchableOpacity>
+            </Box>
+          );
+        })}
+      </Box>
+    </>
   );
 };
 export default BottomTabs;
